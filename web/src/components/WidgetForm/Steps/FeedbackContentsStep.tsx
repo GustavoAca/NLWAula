aa614d5388cf1755933287bc5c01d912db069ac7
Gustavo Aca <galasdalas50@gmail.com>
2022-05-09 10:47:28 -0300

    Projeto configurado para consumir API

--- web/src/components/WidgetForm/Steps/FeedbackContentsStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentsStep.tsx
@@ -3,6 +3,8 @@ import { FeedbackType, feedbackTypes } from ".."
 import { ArrowLeft, Camera } from "phosphor-react";
 import { ScreenshotButton } from "../ScreenshotButton";
 import { FormEvent, useState } from 'react';
+import { api } from "../../../lib/api";
+import { Loading } from "../../Loading";
 
 
 interface FeedbackContentStepProps {
@@ -19,15 +21,27 @@ export function FeedbackContentStep({
 
   const [comment, setComment] = useState('');
   const [screenshot, setScreenshot] = useState<string | null>(null);
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false);
 
   const feedbackTypeInfo = feedbackTypes[feedbackType];
 
-  function handleSubmitFeedback(event: FormEvent) {
+  async function handleSubmitFeedback(event: FormEvent) {
     event.preventDefault();
+
+setIsSendingFeedback(true);
+/*
     console.log({
       screenshot,
       comment,
+    })*/
+
+   await api.post('/feedbacks',{
+      type: feedbackType,
+      comment,
+      screenshot,
+
     })
+    setIsSendingFeedback(false);
    onFeedbackSent();
   }
   return (
@@ -61,10 +75,10 @@ export function FeedbackContentStep({
             onScreenshotTook={setScreenshot} />
 
           <button
-            disabled={comment.length === 0}
+            disabled={comment.length === 0 || isSendingFeedback}
             type="submit"
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500">
-            Enviar Feedback
+            {isSendingFeedback ? <Loading/> : 'Enviar Feedback'}
 
           </button>
         </footer>
