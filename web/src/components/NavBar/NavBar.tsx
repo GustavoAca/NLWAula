9fe1cbd098571faf38dbdd27170397ed8b5f79e2
Gustavo Aca <galasdalas50@gmail.com>
2022-05-10 14:10:55 -0300

    Criação de navbar

--- web/src/components/NavBar/NavBar.tsx
+++ web/src/components/NavBar/NavBar.tsx
@@ -1,10 +1,18 @@
+import { GithubLogo } from 'phosphor-react';
 import './NavBar.css';
 
 export function NavBar() {
   return (
     <header>
       <nav className='container'>
-        <div className='nav-content'>
+        <div className='dsmovie-nav-content'>
+          <h1>Widget de suporte </h1>
+          <a href="https://github.com/GustavoAca">
+            <div className='dsmovie-contact-container'>
+              <GithubLogo className="w-20h-20" />
+              <p className='dsmovie-contact-link'>/GustavoAca</p>
+            </div>
+          </a>
         </div>
       </nav>
     </header>
